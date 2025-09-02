e7740d524a766a45a498a7f5ea81e6736165c7bb
rudrarathore169 <rpsr213316@gmail.com>
2025-09-02 11:52:14 +0530

    java script basic

---
+++ day11/script.js
@@ -0,0 +1,43 @@
+// console.log("hello world")
+
+// const c = 200;
+// console.log(c);
+
+let $firstName = "rudra pratap";
+console.log($firstName);
+
+//snake case,camel case , passcle case
+
+//var, let, const
+
+//premitive types - number, string, boolean, undefined, symbol, null
+//ref type - array, objects, date
+
+// let num1 = -100.42;
+// console.log(num1,"is a",typeof num1);
+
+// let str1 = `hello , My name is rudra`;
+// console.log(str1);
+
+// let firstName = 'rudra';
+// let str2 = `Hello , My Name is ${firstName}`;
+
+// console.log(str2);
+
+// let newvar = Symbol("p1");
+// let newvar2 = Symbol("p2");
+
+// console.log(newvar);
+// console.log(newvar2);
+
+let obj={
+
+    firstName:"rudra",
+    lastname :"pratap",
+}
+
+console.log(typeof obj);
+
+let date = new Date();
+
+console.log(date);
