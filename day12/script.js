62d21d044ae22ef0ebf96a7889bbfbeacfbe1a1a
rudrarathore169 <rpsr213316@gmail.com>
2025-09-03 11:49:11 +0530

    operators/functions/loops in js

---
+++ day12/script.js
@@ -0,0 +1,158 @@
+// arithmetic operators
+
+// let x=5;
+// let y="5";
+
+// console.log("the sum is ",x+y);
+// console.log(y-x);
+// console.log(x**y);
+
+// let a = "rudra";
+
+// x=x+y;
+// console.log(x);
+
+// console.log(x!=y);
+// let num1 = 10;
+// let num2 = 0;
+// let num3 = 20;
+
+// console.log(num1 && num3); 
+// console.log(num2 || num3);  
+// console.log(num1 || num2); 
+// console.log(num2 && num3);
+
+// let str1 = "Hello";
+// let str2 = "";
+// let num1 = 42;
+
+// console.log(str1 && num1); 
+// console.log(str2 || num1); 
+// console.log(str1 || str2); 
+// console.log(num1 && str2);
+
+// let isTrue = true;
+// let isFalse = false;
+// let num = 10;
+
+// console.log(isTrue && num);
+// console.log(isFalse || num); 
+// console.log(isTrue && isFalse);   
+// console.log(isFalse || isTrue);
+
+// let num1 = 0;
+// let num2 = 100;
+// let str1 = "JS";
+// let str2 = "";
+
+// console.log((num1 || num2) && str1); 
+// console.log((str2 || num1) || str1); 
+// console.log((str1 && num2) || num1);
+// console.log((num1 && str2) || str1);
+
+// let bool1 = true;
+// let bool2 = false;
+// let num = 0;
+// let str = "Code";
+
+// console.log(bool1 && str && num);  
+// console.log(bool1 || str || num);    
+// console.log(bool2 || num || str);    
+// console.log(bool2 && str && num);
+
+// let a = 5;
+// let b = 0;
+// let c = "Hi";
+// let d = "";
+
+// console.log(a && b && c); 
+// console.log(a || b || d); 
+// console.log(b || d || c); 
+// console.log(c && a && d);
+
+// let x = 10;
+// let y = 0;
+// let z = -5;
+
+// console.log(x && y && z); 
+// console.log(x || y || z); 
+// console.log(y || z || x); 
+// console.log(z && x && y);
+
+// console.log(x<<3);
+// console.log(x>>1);
+
+// let day=1;
+
+// switch (day) {
+//     case 1:
+//         console.log("today is monday");
+
+//         break;
+//     case 2:
+//         console.log("tuesday");
+//         break;
+//     case 3:
+//         console.log("wednesday");
+//         break;
+//     case 4:
+//         console.log("thursday");
+//         break;
+//     case 5:
+//         console.log("friday");
+//         break;
+
+//     case 6:
+//         console.log("saturday");
+//         break;
+//     case 7:
+//         console.log("sunday");
+//         break;
+
+//     default:
+//         break;
+// }
+
+// function func(a,b){
+//     console.log(`the value of a is ${a}, the value of b is ${b}`);
+// }
+
+// func(100,200)
+
+// const func1=function(a,b){
+//     console.log(a,b);
+// }
+
+// func1(100,200)
+
+// const func1 = (a,b)=>{
+//     console.log(a,b);
+// }
+
+// func1(100,200);
+
+// loops
+
+function func(n){
+    let stars="";
+    let gstars="";
+        for(let j=0;j<n;j++){
+            stars+="*";
+            if(j==0||j==n-1){
+               gstars+="*";
+            }
+            else{
+                gstars+=" ";
+            }
+        }
+    for(let i=0;i<n;i++){ 
+        if(i==0||i==n-1)
+        console.log(stars);
+
+        else{
+            console.log(gstars);
+        }
+    }
+}
+
+func(6);
---
+++ day12/tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+function func(a,b){
+    console.log(`the value of a is ${a}, the value of b is ${b}`);
+}
+
+func(100,200)
